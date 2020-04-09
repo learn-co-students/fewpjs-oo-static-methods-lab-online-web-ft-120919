af63de3c3a6356cea450801b604dcde4091f51b4
ben20262 <ben20262@aim.com>
2020-04-09 16:01:44 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 class Formatter {
-  //add static methods here
+  
+  static capitalize(string) {
+    let letter = string[0].toUpperCase()
+    return letter.concat('', string.slice(1))
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^0-9a-zA-Z-' ]/gi, '')
+  }
+
+  static titleize(string) {
+    let array = string.split(' ')
+    const noNoWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    return array.map((word, index) => {
+      if (!noNoWords.includes(word) || index === 0) {
+        let letter = word[0].toUpperCase()
+        return letter.concat('', word.slice(1))
+      } else {
+        return word
+      }
+    }).join(' ')
+  }
+
 }
