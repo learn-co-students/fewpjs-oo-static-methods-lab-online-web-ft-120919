5bec7734babe3c70317bc121c541b4a6dba6dd4e
Francesca Dreith <francescadreith@Francescas-MacBook-Air.local>
2020-04-09 13:22:22 -0700

    Done.

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 class Formatter {
   //add static methods here
   static capitalize(string) {
-    return string.capitalize
+    return string.charAt(0).toUpperCase() + string.slice(1)
   }
 
-  static sanitize() {
-
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-'' ']+/g, '')
   }
 
-  static titleize() {
-
+  static titleize(string) {
+    let execeptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let titlecase = []
+    let words = string.split(' ')
+    for (let i = 0; i < words.length; i++) {
+      if (i == 0) {
+        titlecase.push(this.capitalize(words[i]))
+      } else {
+        if (execeptions.includes(words[i])) {
+          titlecase.push(words[i])
+        } else {
+          titlecase.push(this.capitalize(words[i]))
+        }
+      }
+    }
+    return titlecase.join(" ")
   }
 }
