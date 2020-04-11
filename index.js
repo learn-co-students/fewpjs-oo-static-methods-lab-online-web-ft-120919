6410168a191590a3558521a8fcb99aa9d8b1950e
Olusola Olagoroye <solaolagoroye@gmail.com>
2020-04-10 20:30:16 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 class Formatter {
   //add static methods here
+  static capitalize(string){
+    return string.slice(0).toUppercase() + string.slice(1)
+
+  }
+
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9-' ]+/g, '');
+  }
 }
